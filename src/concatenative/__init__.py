f5b0cb3db32025b4a740fb3c9036c91ee5a071b6
matthewh806 <matthewh806@gmail.com>
2025-12-30 17:48:02 +0100

    Make main src dir a module

---
+++ src/concatenative/__init__.py
@@ -0,0 +1,7 @@
+from .concatenative_synth import concatenate_snippets, run_download_backend, run_dir_backend
+
+__all__ = [
+    "concatenate_snippets"
+    "run_download_backend"
+    "run_dir_backend"
+]
