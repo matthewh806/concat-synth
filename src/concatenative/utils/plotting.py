03627654563e0c12905f4a1a72530d87d913e253
matthewh806 <matthewh806@gmail.com>
2026-01-04 14:47:03 +0100

    Add annotation on hover logic

--- src/concatenative/utils/plotting.py
+++ src/concatenative/utils/plotting.py
@@ -46,7 +46,7 @@ class InteractiveCorpusPlot:
         self.title = title
 
         self.fig, self.ax = plt.subplots(figsize=(12,10))
-        scatter = self.ax.scatter(
+        self.scatter = self.ax.scatter(
             x = self.x_data,
             y = self.y_data,
             c = self.colour_data,
@@ -59,13 +59,51 @@ class InteractiveCorpusPlot:
         self.ax.set_ylabel(f"{y_axis_feature}", fontsize=12)
         self.ax.grid(True, which='both', linestyle='--', linewidth=0.5)
 
-        cbar = self.fig.colorbar(scatter, ax=self.ax)
+        cbar = self.fig.colorbar(self.scatter, ax=self.ax)
         cbar.set_label(f"Color: {colour_feature}", fontsize=12)
 
         # -- Step 5: Add an annotation object to display Snippet details
         self.annot = self.ax.annotate(
-            "", xy=(0,0), xytext=(15,15), textcoords='offset points',
-
+            "", xy=(0,0), xytext=(15,15), textcoords='offset points'
         )
 
+        # -- Step 6: Connect event handlers
+        self.fig.canvas.mpl_connect("motion_notify_event", self.hover)
+
         self.annot.set_visible(False)
+
+    def update_annot(self, index):
+        '''
+        Updates the annotations text and position.
+        '''
+        pos = self.scatter.get_offsets()[index]
+        self.annot.xy = pos
+
+        snippet = self.snippets[index]
+
+        feature_filename = snippet.metadata['filename'] if snippet.metadata['filename'] else ""
+        feature_txt = "\n".join([f"{k}: {v:.2f}" for k, v in snippet.features.items()])
+
+        self.annot.set_text(f"ID: {snippet.id}\nfilename: {feature_filename}\n{feature_txt}")
+
+
+    def hover(self, event):
+        '''
+        Handles mouse hover events (motion_notify_event)
+        
+        :param event: associated event (MouseEvent)
+        '''
+
+        is_visible = self.annot.get_visible()
+        if event.inaxes == self.ax:
+            contains, details = self.scatter.contains(event)
+            if contains:
+                # Gets the index of the point under the mouse
+                index = details['ind'][0]
+                self.update_annot(index)
+                self.annot.set_visible(True)
+                self.fig.canvas.draw_idle()
+            else:
+                if is_visible:
+                    self.annot.set_visible(False)
+                    self.fig.canvas.draw_idle()
