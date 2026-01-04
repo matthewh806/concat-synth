f9181ddf0a4c2c93b6d81e093dd6612e51cb7790
matthewh806 <matthewh806@gmail.com>
2026-01-04 15:47:36 +0100

    Playback audio on click from plotting script

--- scripts/feature_map_plotting.py
+++ scripts/feature_map_plotting.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import matplotlib.pyplot as plt
-from concatenative.core import audio_loader
+import sounddevice as sd
+from concatenative.core import audio_loader, AudioSnippet
 from concatenative.core.corpus import Corpus
 from concatenative.utils.plotting import InteractiveCorpusPlot
 from concatenative.core.logger import setup_logger
@@ -8,6 +9,25 @@ import logging
 import sys
 
 ROOT = Path(__file__).resolve().parents[1]
+
+def play_snippet_audio_callback(snippet):
+    '''
+    Callback method which is passed to InteractiveCorpusPlot
+    Will be called when a point in the scatter plot is clicked
+    And uses sounddevice to playback the snippets audio
+    
+    :param snippet: AudioSnippet to playback
+    '''
+
+    if not snippet:
+        return
+    
+    try:
+        sd.stop() 
+        sd.play(snippet.samples, snippet.sample_rate, blocking=False)
+    except Exception as e:
+        print(f"Error playing audio: {e}")
+
 
 if __name__ == "__main__":
 
@@ -23,5 +43,5 @@ if __name__ == "__main__":
     snippets = [snip for path in file_paths if (snip := audio_loader(path, max_clip_length=0.2)) is not None]
     corpus = Corpus(snippets)
 
-    plot = InteractiveCorpusPlot(corpus.snippets, 'rms', 'spectral_centroid', 'pitch')
+    plot = InteractiveCorpusPlot(corpus.snippets, 'rms', 'spectral_centroid', 'pitch', on_click_callback=play_snippet_audio_callback)
     plt.show()
