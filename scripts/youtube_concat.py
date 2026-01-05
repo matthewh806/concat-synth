0d0927978fff70b7da9baefb233175ab5fd228f9
matthewh806 <matthewh806@gmail.com>
2026-01-05 20:23:05 +0100

    Add logger to youtube_concat script

--- scripts/youtube_concat.py
+++ scripts/youtube_concat.py
@@ -1,7 +1,12 @@
 from pathlib import Path
 from concatenative import run_download_backend
+from concatenative.utils.logger import setup_logger
+import logging
 
 ROOT = Path(__file__).resolve().parents[1]
 
 if __name__ == "__main__":
+    setup_logger(log_level=logging.DEBUG)
+    logger = logging.getLogger(__name__)
+
     run_download_backend("youtube", ROOT / "data/words.txt", ROOT / "output.wav", output_length=60, max_snippets=256, max_snippet_length=0.3)
