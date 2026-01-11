e903b5b75fdc28e70ff5922c070e787fda99de9b
matthewh806 <matthewh806@gmail.com>
2026-01-11 19:23:16 +0100

    Use FEATURE_REGISTRY in scripts

--- scripts/feature_map_plotting.py
+++ scripts/feature_map_plotting.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import sounddevice as sd
 from concatenative.audio import audio_loader
 from concatenative.analysis.corpus import Corpus
-from concatenative.analysis.features import Feature
+from concatenative.analysis.available_features import FEATURE_REGISTRY
 from concatenative.analysis.feature_extractor import FeatureExtractor
 from concatenative.path.selector import generate_concatenation_path
 from concatenative.visualisation.plotting import InteractiveCorpusPlot
@@ -11,25 +11,7 @@ from concatenative.utils.logger import setup_logger
 import logging
 import sys
 
-import numpy as np
-import librosa
-
 ROOT = Path(__file__).resolve().parents[1]
-
-features = [
-    Feature(
-        name='rms',
-        extractor = lambda samples, _ : np.mean(librosa.feature.rms(y = samples))
-    ),
-    Feature(
-        name='pitch',
-        extractor = lambda samples, sr : np.mean(librosa.pyin(y=samples, fmin=50, fmax=5000, sr=sr))
-    ),
-    Feature (
-        name="spectral centroid",
-        extractor = lambda samples, sr : np.mean(librosa.feature.spectral_centroid(y=samples, sr=sr))
-    )
-]
 
 def play_snippet_audio_callback(snippet):
     '''
@@ -62,8 +44,12 @@ if __name__ == "__main__":
         logger.error(f"No audio files found!")
         sys.exit(1)
 
+    feature_set = [
+        FEATURE_REGISTRY['rms'], FEATURE_REGISTRY['spectral centroid'], FEATURE_REGISTRY['pitch']
+    ]
+
     snippets = [snip for path in file_paths if (snip := audio_loader(path, max_clip_length=0.2)) is not None]
-    corpus = Corpus(snippets, FeatureExtractor(features=features))
+    corpus = Corpus(snippets, FeatureExtractor(features=feature_set))
     print(f"Number of duplicate snippets: {corpus.get_number_of_duplicates()}")
     concatenation_path = generate_concatenation_path(corpus=corpus, output_length_sec=20)
 
--- scripts/nearest_neighbour_calc.py
+++ scripts/nearest_neighbour_calc.py
@@ -1,14 +1,11 @@
 from concatenative.audio import audio_loader
 from concatenative.analysis.corpus import Corpus
-from concatenative.analysis.features import Feature
+from concatenative.analysis.available_features import FEATURE_REGISTRY
 from concatenative.analysis.feature_extractor import FeatureExtractor
 from concatenative.utils.logger import setup_logger
 from pathlib import Path
 import sys
 import logging
-
-import numpy as np
-import librosa
 
 ROOT = Path(__file__).resolve().parents[1]
 
@@ -16,21 +13,6 @@ ROOT = Path(__file__).resolve().parents[1]
 This script loads all of the files in the audio_downloads directory, analyses each AudioSnippet
 and performs a nearest neighbour search using a random AudioSnippet from the list
 '''
-
-features = [
-    Feature(
-        name='rms',
-        extractor = lambda samples, _ : np.mean(librosa.feature.rms(y = samples))
-    ),
-    Feature(
-        name='pitch',
-        extractor = lambda samples, sr : np.mean(librosa.pyin(y=samples, fmin=50, fmax=5000, sr=sr))
-    ),
-    Feature (
-        name="spectral centroid",
-        extractor = lambda samples, sr : np.mean(librosa.feature.spectral_centroid(y=samples, sr=sr))
-    )
-]
 
 if __name__ == "__main__":
 
@@ -43,8 +25,12 @@ if __name__ == "__main__":
         logging.error(f"No audio files found!")
         sys.exit(1)
 
+    feature_set = [
+        FEATURE_REGISTRY['rms'], FEATURE_REGISTRY['spectral centroid'], FEATURE_REGISTRY['pitch']
+    ]
+
     snippets = [snip for path in file_paths if (snip := audio_loader(path, max_clip_length=0.2)) is not None]
-    corpus = Corpus(snippets, FeatureExtractor(features=features))
+    corpus = Corpus(snippets, FeatureExtractor(features=feature_set))
     target = corpus.get_random_snippet()
 
     logging.info(f"Finding nearest neighbour for target: {target}")
