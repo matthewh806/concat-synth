bca361a98ade3bc5424f011b0eea1f2a3f5be22e
matthewh806 <matthewh806@gmail.com>
2026-02-15 14:01:50 +0100

    Update scripts to use methods from common

--- scripts/feature_distribution_plotting.py
+++ scripts/feature_distribution_plotting.py
@@ -1,42 +1,17 @@
 from pathlib import Path
-from concatenative.audio.audio_loader import audio_loader, find_audio_files_recursively
-from concatenative.analysis.corpus import Corpus
-from concatenative.analysis.available_features import FEATURE_REGISTRY
-from concatenative.analysis.feature_extractor import FeatureExtractor
-from concatenative.config import load_config
 from concatenative.visualisation.plotting import plot_corpus_feature_distribution
-from concatenative.utils.logger import setup_logger
-import logging
-import sys
+from common import setup_and_run_synthesis, get_feature_set, get_parser
 
 ROOT = Path(__file__).resolve().parents[1]
 
-FEATURE = 'pitch'
-
 if __name__ == "__main__":
 
-    setup_logger(log_level=logging.DEBUG)
-    logger = logging.getLogger(__name__)
+    parser = get_parser()
+    args = parser.parse_args()
 
-    audio_dir = Path(ROOT / "audio_downloads")
-    file_paths = find_audio_files_recursively(audio_dir)
+    corpus, concat_path, config = setup_and_run_synthesis(args.config)
+    print(concat_path.get_stats())    
+    feature_set = get_feature_set(config)
 
-    if len(file_paths) == 0:
-        logger.error(f"No audio files found!")
-        sys.exit(1)
-
-    if FEATURE not in FEATURE_REGISTRY:
-        logger.error(f"Feature {FEATURE} is not supported!")
-        sys.exit(1)
-
-    config = load_config()
-    feature_set = [FEATURE_REGISTRY[FEATURE]]
-    snippets = [
-        snippet
-        for file_path in file_paths
-        for snippet in audio_loader(
-            file_path, max_clip_length = 0.2, segmentation_stratgy='slices', max_snippets=1, config=config
-        )
-    ]
-    corpus = Corpus(snippets, FeatureExtractor(features=feature_set, config=config))
-    plot_corpus_feature_distribution(corpus, feature=feature_set[0], bins=100)
+    for feature in feature_set:
+        plot_corpus_feature_distribution(corpus, feature=feature, bins=100)
--- scripts/nearest_neighbour_calc.py
+++ scripts/nearest_neighbour_calc.py
@@ -1,11 +1,5 @@
-from concatenative.audio.audio_loader import audio_loader, find_audio_files_recursively
-from concatenative.analysis.corpus import Corpus
-from concatenative.analysis.available_features import FEATURE_REGISTRY
-from concatenative.analysis.feature_extractor import FeatureExtractor
-from concatenative.config import load_config
-from concatenative.utils.logger import setup_logger
+from common import setup_corpus, get_parser
 from pathlib import Path
-import sys
 import logging
 
 ROOT = Path(__file__).resolve().parents[1]
@@ -17,28 +11,9 @@ and performs a nearest neighbour search using a random AudioSnippet from the lis
 
 if __name__ == "__main__":
 
-    setup_logger(log_level=logging.DEBUG)
-
-    audio_dir = Path(ROOT / "audio_downloads")
-    file_paths = find_audio_files_recursively(audio_dir)
-
-    if len(file_paths) == 0:
-        logging.error(f"No audio files found!")
-        sys.exit(1)
-
-    feature_set = [
-        FEATURE_REGISTRY['rms'], FEATURE_REGISTRY['spectral centroid'], FEATURE_REGISTRY['pitch']
-    ]
-
-    config = load_config()
-    snippets = [
-        snippet
-        for file_path in file_paths
-        for snippet in audio_loader(
-            file_path, max_clip_length = 0.2, segmentation_stratgy='slices', max_snippets=1, config = config
-        )
-    ]
-    corpus = Corpus(snippets, FeatureExtractor(features=feature_set, config = config))
+    parser = get_parser()
+    args = parser.parse_args()
+    corpus, config = setup_corpus(config_path=args.config)
     target = corpus.get_random_snippet()
 
     logging.info(f"Finding nearest neighbour for target: {target}")
