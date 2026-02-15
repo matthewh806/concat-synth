688c2f4a35a0f8eb9fdaf4f1d2889660ee1e9f8a
matthewh806 <matthewh806@gmail.com>
2026-02-15 13:12:56 +0100

    Extract corpus setup logic to common file

---
+++ scripts/common.py
@@ -0,0 +1,49 @@
+from typing import Tuple
+from concatenative.analysis.corpus import Corpus
+from concatenative.path.concatenation_path import ConcatenationPath
+from concatenative.analysis.available_features import FEATURE_REGISTRY
+from concatenative.analysis.feature_extractor import FeatureExtractor
+from concatenative.config import load_config
+from concatenative.utils.logger import setup_logger
+from concatenative.audio.audio_loader import find_audio_files_recursively, audio_loader
+import logging
+import sys
+from pathlib import Path
+
+logger = logging.getLogger(__name__)
+
+def setup_corpus(config_path: str) -> Corpus:
+
+    setup_logger(log_level=logging.DEBUG)
+    config = load_config(config_path)
+
+    corpus_path = config['input']['corpus_path']
+    file_paths = find_audio_files_recursively(Path(corpus_path))
+
+    if len(file_paths) == 0:
+        logger.error(f"No audio files found!")
+        sys.exit(1)
+
+    feature_set = []
+    feature_names = config['features']['names']
+    for feature_name in feature_names:
+        if feature_name not in FEATURE_REGISTRY:
+            logger.error(f"Feature {feature_name} is not supported!")
+            sys.exit(1)
+
+        feature_set.append(FEATURE_REGISTRY[feature_name])
+
+    snippets = [
+        snippet
+        for file_path in file_paths
+        for snippet in audio_loader(
+            file_path, max_clip_length = 0.2, segmentation_stratgy='slices', max_snippets=1, config=config
+        )
+    ]
+
+    return Corpus(snippets, FeatureExtractor(features=feature_set, config=config))
+
+
+def setup_and_run_synthesis(config_path: str) -> Tuple[Corpus, ConcatenationPath]:
+    corpus = setup_corpus(config_path)
+    return generate_freeform_path(corpus=corpus, output_length_sec=30)
